664eb27e7622e0ce12d8aab6719b402bff284449
GiauVo <vtngocgiau@gmail.com>
2024-07-03 19:38:47 +0700

    develop

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-#Calculate a, b
-def sum (a, b):
+# Calculate a, b
+def sum(a, b):
     return a + b
 
+
+def divide(a, b):
+    return (a/b)
+
+
 if __name__ == "__main__":
-    print(f'Total: {sum (1,5)}')
+    print(f'Total: {sum(1, 5)}')
